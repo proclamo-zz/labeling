60a5d8a7e9c5dbb80439be9f729486bdd0f72f01
Cristian Martin Mouat <proclamo@gmail.com>
2014-07-21 11:28:04 +0200

    Commit initial

--- README.md
+++ README.md
@@ -1,4 +1,76 @@
 labeling
 ========
 
-d3 plugin for automatic labeling
+A [d3.js](https://github.com/mbostock/d3) plugin for automatic labeling. It tries to resolve [the automatic label placement problem](http://en.wikipedia.org/wiki/Automatic_label_placement) with an heuristic aproach.
+
+
+#INSTALL
+
+You only have to include the script after calling d3 library.
+
+	<script src="http://d3js.org/d3.v3.min.js"></script>
+	<script type="text/javascript" src="labeling.js"></script>
+
+#HOW IT WORKS
+
+Each overlapped label rotates around its position until no more overlaps. If one label reach the last position there are many options: 
+
+* pass(): do nothing. If you consider it can be no overlaps in the next round you can just let it recalculate.
+* scale( *factor* ): the label scales *factor* times and run again the algorithm. *scale* can be called multiple times with different parameters. Defaults to .75.
+* legend(): transforms the label into a number and builds an internally legend which can be returned with the getLegend() function.
+* remove(): the label is removed.
+* custom(fn): fn is your own function. Receives the current label as a parameter. The *this* object is the labeling object.
+
+The basic functioning is shown here:
+
+
+	d3.labeling()
+	  .select('.label')
+	  .align()
+	  
+select can be a d3's valid string selector or a function which returns a d3 selection:
+
+	d3.labeling()
+	  .select(function() { 
+         return d3.selectAll('.place-label')
+           .sort(function(a,b) { 
+               return d3.geo.area(b.geometry) - d3.geo.area(a.geometry); 
+           })
+      })
+      .align();
+      
+#EXAMPLES
+      
+For example we have:
+
+	d3.labeling()
+      .select('.place-label')  // className of the labels
+      .legend()				    // builds a legend
+      .scale(.8)			    // if no fit, scales down 0.8 times the label (or number)
+      .scale(.5)			    // idem but scales 0.5 times    
+      .remove()				    // else removes
+      .align();      			// start
+      
+You can view this example live [in a block](http://bl.ocks.org/proclamo/0fc304040b7036eb7785).
+
+Two passes:
+
+	d3.labeling()
+	  .select('.label')
+	  .pass()
+	  .align();
+	  
+Remark the affecteds labels:
+
+	d3.labeling()
+	  .select('.label')
+	  .custom(function(label){
+	  	label.attr('stroke', 'red');
+	  })
+	  .align();   
+	  
+#TODO
+
+* add support for rigth to left languages
+* the actual radius for rotating the label is derived from the bounding box, this radius might be configurable.
+* improve the calculus of remaining overlappeds.
---
+++ labeling.js
@@ -0,0 +1,199 @@
+;
+d3.labeling = function() {
+
+	var labeling = {}, className, labels, callbacks = [], callbackpos = -1,
+		updateLabels = false, _break = false, legend = [];	
+
+	labeling.select = function(_className) {
+		className = _className;
+		labels = getLabels();
+		return labeling;
+	}
+
+	var getLabels = function() {
+
+		if (typeof className === "string") {
+			return svg.selectAll(className);	
+		}
+		else {
+			return className.call(this);
+		}
+	}
+
+	labeling.remove = function() {
+		callbacks.push(labeling_remove);	
+		updateLabels = true;
+		return labeling;
+	}
+
+	labeling.scale = function(_scaleFactor) {
+		
+		var scaleFactor = _scaleFactor || .75;
+
+		// currying: https://medium.com/@kbrainwave/currying-in-javascript-ce6da2d324fe
+		var fn = function(label) {
+			return labeling_scale(label, scaleFactor);
+		}
+
+		callbacks.push(fn);
+
+		return labeling;
+	}
+
+	labeling.legend = function() {
+		callbacks.push(labeling_legend);
+		updateLabels = true;
+		return labeling;
+	}
+
+	labeling.pass = function() {
+		callbacks.push(function(){});
+		return labeling;
+	}
+
+	labeling.custom = function(fn) {
+		callbacks.push(fn);
+		return labeling;
+	}
+
+	var labeling_remove = function(label) {
+		label.remove();
+	}
+
+	var labeling_scale = function(label,factor) {
+
+		var x = +label.attr("x"), y = +label.attr("y"),
+			scaleFactor = factor < 1 ? 1 - factor : - (factor - 1),
+			transform = "translate(" + (x * scaleFactor) + "," + (y * scaleFactor) + ") scale(" + factor + ")";
+
+		label.attr("transform", transform);
+	}
+
+	var labeling_legend = function(label) {
+
+		legend.push(label.text());
+
+		label.text(legend.length);
+
+	}
+
+	var next = function(label) {
+
+		var box = getBox(label);
+
+		var pos = label.attr("data-pos") || 0;
+
+		pos++;
+
+		if (pos === 8) {
+
+			var calls = label.attr("data-calls") || -1;
+
+			calls++;
+
+			label.attr("data-calls", calls);
+
+			if (calls < callbacks.length) {
+				var fn = callbacks[calls];
+				fn.call(labeling, label);
+			}
+
+			pos = 0;
+			box = getBox(label);
+
+			// prevent infinite loop
+			if (!callbacks.length) {
+				_break = true;
+			}
+
+		}
+
+		label.attr("data-pos", pos);
+
+		var x = +label.attr("dx") || 0, y = +label.attr("dy") || 0;
+
+		switch(pos) {
+			case 0:
+			case 1:
+				y += box.height/2;
+				break;
+			case 2:
+			case 3:
+				x -= box.width/2;
+				break;
+			case 4:
+			case 5:
+				y -= box.height/2;
+				break;
+			case 6: 
+			case 7:	
+				x += box.width/2;
+				break;			
+		}
+
+		label.attr("dx", x).attr("dy", y);
+
+	}
+
+	var overlaps = function(a, b) {
+		
+		return (
+			(a.left <= b.left && b.left <= a.right)
+			||
+			(a.left <= b.right && b.right <= a.right)
+		)	
+		&&
+		(
+			(a.top <= b.top && b.top <= a.bottom)
+			||
+			(a.top <= b.bottom && b.bottom <= a.bottom)
+		);
+	}	
+
+	var getBox = function(d) {
+		return d[0][0].getBoundingClientRect();
+	}
+
+	labeling.align = function() {
+
+		var overlappeds;
+		
+		do {
+			overlappeds = 0;
+
+			labels.each(function() {
+				var current = this;
+				var box_text = getBox(d3.select(current));
+
+				var _overlappeds = labels[0].filter(function(d) {
+					if (d === current) return false;
+					return overlaps(box_text, getBox(d3.select(d)));
+				});
+
+				overlappeds += _overlappeds.length;
+
+				_overlappeds.forEach(function(el) {
+					next(d3.select(el));
+				});
+
+			});
+
+			if (updateLabels) {
+				labels = getLabels(className);
+			}
+
+		} while (overlappeds > 0 && !_break);
+		
+		return labeling;
+	}
+
+	labeling.getLegend = function() {
+
+		return legend.map(function(d, i) {
+			return { key: i + 1, name: d }
+		});
+	}
+
+	return labeling;
+
+}
